db9e69fa20b06b679ccef2bab3e2b6176822dece
Alexes Presswood <ampress727@gmail.com>
2018-01-05 13:02:15 -0600

    Add GetPerson Endpoint

--- first-go-api/firstApi.go
+++ first-go-api/firstApi.go
@@ -21,6 +21,17 @@ type Address struct {
 
 var people[]Person
 
+func GetPersonEndpoint(writer http.ResponseWriter, request *http.Request) {
+	params := mux.Vars(request)
+	for item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(writer).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(writer).Encode(&Person{})
+}
+
 func main() {
 	people = append(people, Person{ID: "1", Firstname: "Alexes", Lastname: "Presswood", Address: &Address{City: "Kansas City", State: "MO"}})
 	people = append(people, Person{ID: "2", Firstname: "Test", Lastname: "User"})
