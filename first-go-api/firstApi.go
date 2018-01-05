ed74a0ec2984c94021b4ec32fd5dcbfe57f75fe0
Alexes Presswood <ampress727@gmail.com>
2018-01-05 12:56:50 -0600

    Populate Objects

--- first-go-api/firstApi.go
+++ first-go-api/firstApi.go
@@ -1,23 +1,27 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
-    "github.com/gorilla/mux"
+	"encoding/json"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
 )
 
 type Person struct {
-    ID        string   `json:"id,omitempty"`
-    Firstname string   `json:"firstname,omitempty"`
-    Lastname  string   `json:"lastname,omitempty"`
-    Address   *Address `json:"address,omitempty"`
+	ID string `json:"id,omitempty"`
+	Firstname string `json:"firstname,omitempty"`
+	Lastname string `json:"lastname,omitempty"`
+	Address *Address `json:"address,omitempty"`
 }
 
 type Address struct {
-    City  string `json:"city,omitempty"`
-    State string `json:"state,omitempty"`
+	City string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
 }
 
+var people[]Person
+
 func main() {
+	people = append(people, Person{ID: "1", Firstname: "Alexes", Lastname: "Presswood", Address: &Address{City: "Kansas City", State: "MO"}})
+	people = append(people, Person{ID: "2", Firstname: "Test", Lastname: "User"})
 }
